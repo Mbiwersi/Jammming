d24a9e2f89e001726e8ccb609ca66bcb1edddb05
Mbiwersi <Mbiwersi>
2021-06-28 01:50:37 -0500

    SearchResults Component

---
+++ src/Components/SearchResults/SearchResults.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './SearchResults.css';
+
+class SearchResults extends React.Component {
+    render() {
+        return (
+            <div className="SearchResults">
+                <h2>Results</h2>
+                {/* <TractList /> */}
+            </div>
+        )
+    }
+}
+
+export default SearchResults;
